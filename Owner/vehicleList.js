2d2dbea3edfd92668762cd34fe119ed362211126
Ravindu Hasanka <rhatu2000@gmail.com>
2023-11-02 15:42:03 +0530

    vehicle add  done

--- Owner/vehicleList.js
+++ Owner/vehicleList.js
@@ -1,26 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
+    const form = document.querySelector("form"),
+    tbody = document.querySelector(".tbody");
   
-    fetch('http://localhost:8080/try2_war_exploded/getVehicle?vehicleNo=dna',{
+    let row ="";
+
+    fetch('http://localhost:8080/try2_war_exploded/viewAllVehicle',{
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
             },})
             .then(response => response.json())
             .then(data => {
-                console.log(data.vehicle) 
-                document.querySelector('.Name').innerHTML = data.vehicle.ownerEmail;  
-                // form.querySelector('.vehicleNumber').value = data.vehicle.vehicleNo;
-                // form.querySelector('.vehicle-type').value = data.vehicle.type;
-                // form.querySelector('.vehicleBrand').value = data.vehicle.vehicleBrand;
-                // form.querySelector('.vehicleModel').value = data.vehicle.model;
-                // form.querySelector('.vehicleRegNo').value = data.vehicle.regNo;
-                // form.querySelector('.driverEmail').value = data.vehicle.driverEmail;
-                // form.querySelector('.seatCount').value = data.vehicle.seatsCount;
-                // form.querySelector('.startingLocation').value = data.vehicle.startingPoint;
-                // form.querySelector('.endLocation').value = data.vehicle.endingPoint;
-                // form.querySelector('.vehicle-trips').value = data.vehicle.trips;
-                // form.querySelector('.vehicleTrips').value = data.vehicle.trips;
+                data.list.forEach(vehical => {
+                row += `<tr>
+                        <td scope="owner" data-label="P ID">`+ vehical.ownerEmail +`</td>
+                        <td class="Name">`+ vehical.vehicleNo +`</td>
+                        <td class="Address">`+ vehical.regNo +`</td>
+                        <td class="NIC">`+ vehical.vehicleBrand +`</td>
+                        <td class="Telephone">`+ vehical.type +`</td>
+                        <td class="Email">`+ vehical.driverEmail +`</td>
+                        <td class="Telephone">`+ vehical.seatsCount +`</td>
+                        <td class="Email">`+ vehical.trips +`</td>
+                        <td class="Email">`+ vehical.startingPoint +`</td>
+                        <td class="Email">`+ vehical.endingPoint +`</td>
+                        <td class="Action">
+                            <a href="#moredetails"><button class="moredetails">MORE</button></a>
+                            <a href="#edit"></a><button class="edit">EDIT</button></a>
+                            <a href="#delete"></a><button class="delete">DELETE</button></a>
+                        </td>
+                    </tr>`
+                });
+
+                document.querySelector(".tbody").innerHTML = row;   
 
             })
             .catch(error => {
@@ -29,3 +40,62 @@ document.addEventListener("DOMContentLoaded", function () {
     
   });
   
+
+
+
+
+
+
+
+// function fetchAllData() {
+//     fetch('http://localhost:15000/ecoswapperbackend_war/admin/centers', {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//     })
+//         .then(response => {
+//             if (response.ok) {
+//                 return response.json();
+//             } else {
+//                 console.error('Error:', response.status);
+//             }
+//         })
+//         .then(data => {
+//             displayDataAsTable(data);
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//         });
+// }
+
+
+// fetchAllData();
+
+// function displayDataAsTable(data) {
+//     const tableBody = document.querySelector("#dataTable tbody");
+
+//     data.forEach(center => {
+//         const row = document.createElement("tr");
+
+//         row.innerHTML = `
+//             <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+//             <td>${center.center_id}</td>
+//             <td>${center.business_name}</td>
+//             <td>${center.business_reg_no}</td>
+//             <td>${center.business_type}</td>
+//             <td>${center.center_bio}</td>
+//             <td>${center.center_location_link}</td>
+//             <td>
+//                 <span class="icon-container">
+//                     <i class="fas fa-pencil-alt" style="color: #ff0202"></i>
+//                 </span>
+//                 <span class="icon-container" style="margin-left: 10px;"> <!-- Adjust the margin as needed -->
+//                     <i class="fas fa-trash-alt" style="color: #ff0202"></i>
+//                 </span>
+//             </td>
+//         `;
+
+//         tableBody.appendChild(row);
+//     });
+// }
