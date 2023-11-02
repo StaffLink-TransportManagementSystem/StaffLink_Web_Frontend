fc2e24b4fed1978a60a8a5abef0b83a80fdc7754
Ravindu Hasanka <rhatu2000@gmail.com>
2023-11-02 17:20:34 +0530

    vehicle add  done

--- Owner/vehicleEditForm.js
+++ Owner/vehicleEditForm.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector("form");
+    const vehicleNo = window.location.href.split("=")[1];
+
+    // console.log(window.location.href.split("=")[1]);
+    
   
-    fetch('http://localhost:8080/try2_war_exploded/getVehicle?vehicleNo=dna',{
+    fetch("http://localhost:8080/try2_war_exploded/getVehicle?vehicleNo=" + vehicleNo ,{
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -21,11 +25,149 @@ document.addEventListener("DOMContentLoaded", function () {
                 form.querySelector('.endLocation').value = data.vehicle.endingPoint;
                 form.querySelector('.vehicle-trips').value = data.vehicle.trips;
                 // form.querySelector('.vehicleTrips').value = data.vehicle.trips;
+                
 
             })
             .catch(error => {
                 console.error('Error:', error);
             });
+            
     
   });
+
+
+  
+  function updatefunction() {
+
+    const form = document.querySelector("form"),
+     sub = document.querySelector(".sub");
+  
+    sub.addEventListener("click", () => {
+    //   event.preventDefault(); // Prevent the default form submission
+        console.log("update form submitted");
+      // Get form elements
+      const ownerEmail = form.querySelector('.ownerEmail').value;
+      const vehicleNumber = form.querySelector('.vehicleNumber').value;
+      const vehicleType = form.querySelector('.vehicle-type').value;
+      const vehicleBrand = form.querySelector('.vehicleBrand').value;
+      const vehicleModel = form.querySelector('.vehicleModel').value;
+      const regNumber = form.querySelector('.vehicleRegNo').value;
+      const driverEmail = form.querySelector('.driverEmail').value;
+      const seatCount = form.querySelector('.seatCount').value;
+      const startingLocation = form.querySelector('.startingLocation').value;
+      const endLocation = form.querySelector('.endLocation').value;
+      const vehicleTrips = form.querySelector('.vehicle-trips').value;
+
+        
+
+  
+      // Validate form data (You can add more validation as needed)
+      if (!ownerEmail ) {
+        alert("Please fill in all required fields.");
+      }
+      else if (!vehicleNumber) {
+        alert("Please fill in all required fields.");
+      }
+      else if(!vehicleType){
+        alert("Please fill in all required fields.");
+      }
+      else if(!vehicleBrand){
+        alert("Please fill in all required fields.");
+      }
+        else if(!vehicleModel){
+            alert("Please fill in all required fields.");
+        }
+        else if(!regNumber){
+            alert("Please fill in all required fields.");
+        }
+        else if(!driverEmail){
+            alert("Please fill in all required fields.");
+        }
+        else if(!seatCount){
+            alert("Please fill in all required fields.");
+        }
+        else if(!startingLocation){
+            alert("Please fill in all required fields.");
+        }
+        else if(!endLocation){
+            alert("Please fill in all required fields.");
+        }
+        else if(!vehicleTrips){
+            alert("Please fill in all required fields.");
+        }
+        else {
+            // All form data are valid, so submit to the server
+           
+  
+      // Prepare the data to send to the backend (You can structure it as needed)
+      const data = {
+        ownerEmail:ownerEmail,
+        vehicleNo:vehicleNumber,
+        type:vehicleType,
+        vehicleBrand:vehicleBrand,
+        model:vehicleModel,
+        regNo:regNumber,
+        driverEmail:driverEmail,
+        seatsCount:seatCount,
+        startingPoint:startingLocation,
+        endingPoint:endLocation,
+        trips:vehicleTrips,
+      };
+  
+      // Simulate an HTTP POST request to a backend endpoint (replace with your actual backend URL)
+      fetch('http://localhost:8080/try2_war_exploded/vehicleEdit',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },body: JSON.stringify(data),})
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.message)
+                window.location.href = "http://127.0.0.1:5501/Owner/ownerDashboard.html";
+                // document.getElementById("demo").innerHTML = data.message;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+      }
+    });
+
+  };
+
+  function deletefunction() {
+
+    const form = document.querySelector("form"),
+     sub = document.querySelector(".sub");
+  
+    sub.addEventListener("click", () => {
+    //   event.preventDefault(); // Prevent the default form submission
+        console.log("update form submitted");
+
+      const vehicleNumber = form.querySelector('.vehicleNumber').value;
+      
+      const data = {
+        vehicleNo:vehicleNumber,}
+
+  
+      // Simulate an HTTP POST request to a backend endpoint (replace with your actual backend URL)
+      fetch('http://localhost:8080/try2_war_exploded/vehicleDelete',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },body: JSON.stringify(data),})
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.message)
+                window.location.href = "http://127.0.0.1:5501/Owner/ownerDashboard.html";
+                // document.getElementById("demo").innerHTML = data.message;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+      }
+    );
+  };
+
+
+
   
