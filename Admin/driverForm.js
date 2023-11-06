c100b1f1c0eb8617e7bff33c851e4ee9ac0cdfe0
Ravindu Hasanka <rhatu2000@gmail.com>
2023-11-06 09:24:13 +0530

    Add owner validation done

--- Admin/driverForm.js
+++ Admin/driverForm.js
@@ -1,3 +1,10 @@
+function isValidEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailRegex.test(email);
+}
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector("form"),
      sub = document.querySelector(".sub");
