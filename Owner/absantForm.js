8a63f3c22ef6e2815af22c326ca5a13b4b2f8398
Ravindu Hasanka <rhatu2000@gmail.com>
2023-11-20 23:13:04 +0530

    add absent: absent form validations and add form done

--- Owner/absantForm.js
+++ Owner/absantForm.js
@@ -73,7 +73,7 @@ function validateNIC(nic) {
         var checker = true;
           
   
-        var ownerEmailError = document.querySelector(".passengerEmail-error-message");
+        var passengerEmailError = document.querySelector(".passengerEmail-error-message");
         var vehicleNoError = document.querySelector(".vehicleNumber-error-message");
         var daysOfAbsantError = document.querySelector(".daysOfAbsant-error-message");
         var startingError = document.querySelector(".startingDate-error-message");
@@ -85,59 +85,69 @@ function validateNIC(nic) {
         // var endLocationError = document.querySelector(".endLocation-error-message");
         // var vehicleTripsError = document.querySelector(".vehicleTrips-error-message");
         
-        ownerEmailError.style.display = "none";
+        passengerEmailError.style.display = "none";
         vehicleNoError.style.display = "none";
         daysOfAbsantError.style.display = "none";
-        startingDate.style.display = "none";
-        endingDate.style.display = "none";
+        startingError.style.display = "none";
+        endingError.style.display = "none";
         
         
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     
         if(!passengerEmail){
-          ownerEmailError.innerText = "Please enter an email address.";
-          ownerEmailError.style.display = "block";
+          passengerEmailError.innerText = "Please enter an email address.";
+          passengerEmailError.style.display = "block";
           console.log("email error");
           checker = false;
         }
         else if (!ValidateEmail(passengerEmail)) {
-          ownerEmailError.innerText = "Please enter a valid email address.";
-          ownerEmailError.style.display = "block";
+          passengerEmailError.innerText = "Please enter a valid email address.";
+          passengerEmailError.style.display = "block";
           console.log("email error");
           checker = false;
         }
+        if(!vehicleNumber){
+            vehicleNoError.innerText = "Please enter a vehicle number.";
+            vehicleNoError.style.display = "block";
+            console.log("vehicle number error");
+            checker = false;
+            }
+        else if (!validateVehicleNumber(vehicleNumber)) {
+            vehicleNoError.innerText = "Please enter a valid vehicle number.";
+            vehicleNoError.style.display = "block";
+            console.log("vehicle number error");
+            checker = false;
+            }
+
         if (!daysOfAbsent) {
-          vehicleNoError.innerText = "Please enter count of absent days.";
-          vehicleNoError.style.display = "block";
+          daysOfAbsantError.innerText = "Please enter count of absent days.";
+          daysOfAbsantError.style.display = "block";
           console.log("Count of absent days error");
           checker = false;
         }
         if (!startingDate) {
-          typeError.innerText = "Please enter a starting date.";
-          typeError.style.display = "block";
+          startingError.innerText = "Please enter a starting date.";
+          startingError.style.display = "block";
           console.log("Starting date error");
           checker = false;
         }
         if (!endingDate) {
-          brandError.innerText = "Please enter a ending date.";
-          brandError.style.display = "block";
+          endingError.innerText = "Please enter a ending date.";
+          endingError.style.display = "block";
           console.log("ending date error");
           checker = false;
         }
         
-        if(checker === false){
-          
-              
+        if(checker === true){              
           const data = {
             passengerEmail: passengerEmail,
-            vehicleNumber: vehicleNumber,
+            vehicleNo: vehicleNumber,
             daysOfAbsent: daysOfAbsent,
             startingDate: startingDate,
             endingDate: endingDate,
           };
-        };
-    
-        // Simulate an HTTP POST request to a backend endpoint (replace with your actual backend URL)
+          console.log(data);
+          // Simulate an HTTP POST request to a backend endpoint (replace with your actual backend URL)
         fetch('http://localhost:8080/try2_war_exploded/addAbsent',{
               method: 'POST',
               headers: {
@@ -146,11 +156,11 @@ function validateNIC(nic) {
               .then(response => response.json())
               .then(data => {
                   console.log(data.message)
-                  window.location.href = "http://127.0.0.1:5501/Admin/absents.html";
-                  // document.getElementById("demo").innerHTML = data.message;
+                  window.location.href = "http://127.0.0.1:5501/Owner/absants.html";
               })
               .catch(error => {
                   console.error('Error:', error);
               });
-        })
-});
+        }
+        });
+        });
