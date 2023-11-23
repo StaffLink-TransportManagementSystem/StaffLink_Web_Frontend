f0ca45519fcac57bb4beb91090c5f55461e411fc
Ravindu Hasanka <rhatu2000@gmail.com>
2023-11-23 11:45:47 +0530

    Owner: abesntList dynamic cards and popup  done

--- Owner/absentListCard.js
+++ Owner/absentListCard.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 </div>
                 <span> ` + v.vehicleNo + `</span>
                 <p class="job">` + v.model + `</p>
-                <button onclick="popup()"> Absent List </button>
+                <button onclick="popup('`+ v.vehicleNo + `')"> Absent List </button>
               </div>`
                 });
 
@@ -59,3 +59,42 @@ document.addEventListener("DOMContentLoaded", function () {
             });
     
   });
+  
+  function popup(vehicleNo){
+    console.log(vehicleNo);
+    let data = {
+        vehicleNo: vehicleNo
+    };
+    let row ="";
+    fetch('http://localhost:8080/try2_war_exploded/viewAbsent',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },body: JSON.stringify(data),})
+        .then(response => response.json())
+        .then(absents => {
+            console.log(absents);
+            absents.list.forEach(absent => {
+                row+=`<tr>
+                <td>`+absent.id+`</td>
+                <td>`+absent.passengerEmail+`</td>
+                <td>`+absent.daysOfAbsent+`</td>
+                <td>`+absent.startingDate+`</td>
+                <td>`+absent.endingDate+`</td>
+            </tr>`
+            }
+            );
+            document.querySelector(".tbody").innerHTML = row;
+            // document.querySelector(".POPUP").style.display = "block";
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    document.querySelector(".tbody").innerHTML = row;
+    document.querySelector(".POPUP").style.display = "block";
+  }
+
+  function closePopup(){
+    document.querySelector(".POPUP").style.display = "none";
+  }
+
