ca479937fba5ad17e9b579f60b0ef0e43ccbf66b
Ravindu Hasanka <rhatu2000@gmail.com>
2023-12-01 23:44:58 +0530

    payment js changed

--- Passenger/Payments/payments.js
+++ Passenger/Payments/payments.js
@@ -1,4 +1,4 @@
-const e = require("express");
+// const e = require("express");
 
 function validateCreditCardNumber(cardNumber) {
     // Remove spaces and non-digit characters
@@ -57,7 +57,7 @@ function validateYear(year) {
     return !isNaN(numericYear) && numericYear >= currentYear && numericYear <= currentYear + 10; // Adjust the range as needed
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+function makePayment() {
     const form = document.querySelector("form"),
     sub = document.querySelector(".sub");
 
@@ -194,7 +194,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 .then(data => {
                     console.log('Success:', data);
                     // alert("Payment added successfully.");
-                    window.location.href = "http://localhost:8080/try2_war_exploded/Passenger/Payments/payments.html";
+                    // window.location.href = "http://localhost:8080/try2_war_exploded/Passenger/Payments/payments.html";
                 })
                 .catch((error) => {
                     console.error('Error:', error);
@@ -202,6 +202,153 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
     });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // const form = document.querySelector("form"),
+    // sub = document.querySelector(".sub");
+
+    // sub.addEventListener("click", () => {
+    //     console.log("form submitted");
+    //     // Get form elements
+    //     const payment = form.querySelector('.payment-input').value;
+    //     const cardNumber = form.querySelector('.card-number-input').value;
+    //     const cardHolder = form.querySelector('.card-holder-input').value;
+    //     const amount = form.querySelector('.amount-input').value;
+    //     const cvv = form.querySelector('.cvv-input').value;
+    //     const month = form.querySelector('.month-input').value;
+    //     const year = form.querySelector('.year-input').value;
+
+    //     var paymentError = document.querySelector(".paymentFor-error-message");
+    //     var cardNumberError = document.querySelector(".cardNo-error-message");
+    //     var cardHolderError = document.querySelector(".holder-error-message");
+    //     var amountError = document.querySelector(".amount-error-message");
+    //     var cvvError = document.querySelector(".cvv-error-message");
+    //     var monthError = document.querySelector(".month-error-message");
+    //     var yearError = document.querySelector(".year-error-message");
+
+    //     paymentError.style.display = "none";
+    //     cardNumberError.style.display = "none";
+    //     cardHolderError.style.display = "none";
+    //     amountError.style.display = "none";
+    //     cvvError.style.display = "none";
+    //     monthError.style.display = "none";
+    //     yearError.style.display = "none";
+
+    //     var checker = true;
+
+    //     if (!payment) {
+    //         paymentError.innerText = "Please select a payment method.";
+    //         paymentError.style.display = "block";
+    //         console.log("payment error");
+    //         checker = false;
+    //     }
+    //     if (!cardNumber) {
+    //         cardNumberError.innerText = "Please enter a card number.";
+    //         cardNumberError.style.display = "block";
+    //         console.log("card number error");
+    //         checker = false;
+    //     }
+    //     else if (!validateCreditCardNumber(cardNumber)) {
+    //         cardNumberError.innerText = "Please enter a valid card number.";
+    //         cardNumberError.style.display = "block";
+    //         console.log("card number error");
+    //         checker = false;
+    //     }
+    //     if (!cardHolder) {
+    //         cardHolderError.innerText = "Please enter a card holder name.";
+    //         cardHolderError.style.display = "block";
+    //         console.log("card holder error");
+    //         checker = false;
+    //     }
+    //     if (!amount) {
+    //         amountError.innerText = "Please enter an amount.";
+    //         amountError.style.display = "block";
+    //         console.log("amount error");
+    //         checker = false;
+    //     }
+    //     else if (!validateCreditCardAmount(amount)) {
+    //         amountError.innerText = "Please enter a valid amount.";
+    //         amountError.style.display = "block";
+    //         console.log("amount error");
+    //         checker = false;
+    //     }
+
+    //     if (!cvv) {
+    //         cvvError.innerText = "Please enter a cvv.";
+    //         cvvError.style.display = "block";
+    //         console.log("cvv error");
+    //         checker = false;
+    //     }
+    //     else if (!validateCVV(cvv)) {
+    //         cvvError.innerText = "Please enter a valid cvv.";
+    //         cvvError.style.display = "block";
+    //         console.log("cvv error");
+    //         checker = false;
+    //     }
+
+    //     if (!month) {
+    //         monthError.innerText = "Please select a month.";
+    //         monthError.style.display = "block";
+    //         console.log("month error");
+    //         checker = false;
+    //     }
+    //     else if (!validateMonth(month)) {
+    //         monthError.innerText = "Please select a valid month.";
+    //         monthError.style.display = "block";
+    //         console.log("month error");
+    //         checker = false;
+    //     }
+
+    //     if (!year) {
+    //         yearError.innerText = "Please select a year.";
+    //         yearError.style.display = "block";
+    //         console.log("year error");
+    //         checker = false;
+    //     }
+    //     else if(year < year.getFullYear()){
+    //         yearError.innerText = "Please select a valid year.";
+    //         yearError.style.display = "block";
+    //         console.log("year error");
+    //         checker = false;
+    //     }
+    //     else if (!validateYear(year)) {
+    //         yearError.innerText = "Please select a valid year.";
+    //         yearError.style.display = "block";
+    //         console.log("year error");
+    //         checker = false;
+    //     }
+
+    //     if(checker){
+    //         const data = {
+    //             paymentFor: payment,
+    //             cardNumber: cardNumber,
+    //             cardHolder: cardHolder,
+    //             amount: amount,
+    //             cvv: cvv,
+    //             month: month,
+    //             year: year
+    //         };
+
+    //         fetch('http://localhost:8080/try2_war_exploded/addPayment', {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             },
+    //             body: JSON.stringify(data)
+    //         })
+    //             .then(response => response.json())
+    //             .then(data => {
+    //                 console.log('Success:', data);
+    //                 // alert("Payment added successfully.");
+    //                 // window.location.href = "http://localhost:8080/try2_war_exploded/Passenger/Payments/payments.html";
+    //             })
+    //             .catch((error) => {
+    //                 console.error('Error:', error);
+    //             });
+    //     }
+
+    // });
 
     passengerEmail = "rhatu2000@gmail.com"
   
@@ -239,6 +386,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 years += `<option value="`+(year+i)+`">`+(year+i)+`</option>`
             }
             console.log(years);
-            document.querySelector(".yearInput").innerHTML = years;
+            document.getElementById("yearInput").innerHTML = years;
     
   });
