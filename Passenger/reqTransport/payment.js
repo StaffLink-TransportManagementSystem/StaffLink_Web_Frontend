cdc19dec76487cd1d95b7237478b54b09aab8e88
Ravindu Hasanka <rhatu2000@gmail.com>
2023-12-13 23:01:54 +0530

    Passenger request: card payment page created

--- Passenger/reqTransport/payment.js
+++ Passenger/reqTransport/payment.js
@@ -9,7 +9,23 @@ function cardPayment(){
     const passengerEmail = urlParams.get('passengerEmail')
     console.log(passengerEmail);
 
-    
+    window.location.href = "./cardPayment.html?vehicleNo="+vehicleNo+"&passengerEmail="+passengerEmail;
+
+    // let data = { vehicleNo: vehicleNo, passengerEmail: passengerEmail };
+    // fetch('http://localhost:8080/try2_war_exploded/passengerCardPayment', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }, body: JSON.stringify(data)
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //     console.log(data);
+    //     location.reload();
+    // })
+    // .catch(error => {
+    //     console.error('Error:', error);
+    // });
 }
 
 function cashPayment(){
@@ -22,4 +38,58 @@ function cashPayment(){
     console.log(vehicleNo);
     const passengerEmail = urlParams.get('passengerEmail')
     console.log(passengerEmail);
+
+    let data = { vehicleNo: vehicleNo, passengerEmail: passengerEmail };
+
+    Swal.fire({
+        title: "Pay to driver?",
+        // text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Pay To Driver",        
+      }).then((result) => {
+        if (result.isConfirmed) {
+            fetch('http://localhost:8080/try2_war_exploded/passengerCashPayment', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }, body: JSON.stringify(data)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.message);
+                if(data.message == "Payment successfully"){
+                    Swal.fire({
+                        title: "Reservation Success!",
+                        icon: "success"
+                    })
+                }
+                
+                
+            })
+            .then(()=>{
+                window.location.href = "./index.html"
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        }
+      });
+
+    // fetch('http://localhost:8080/try2_war_exploded/passengerCashPayment', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }, body: JSON.stringify(data)
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //     console.log(data);
+    //     location.reload();
+    // })
+    // .catch(error => {
+    //     console.error('Error:', error);
+    // });
 }
