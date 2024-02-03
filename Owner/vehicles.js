be52a3d56a58b6649c8ec245ac8af0d9dc2cef74
MadhushanAndawaththa <madhushanandawaththa29@gmail.com>
2024-02-04 00:10:06 +0530

    search function implemeted for all

--- Owner/vehicles.js
+++ Owner/vehicles.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector("form"),
     sub = document.querySelector(".sub");
+    const searchInput = document.querySelector("[vehicle-search]")
+
     console.log("vehicles");
     let email = "rhatu2000@gmail.com"
     let data = { email: email };
     let row ="";
+    let users = []
+
 
     fetch('http://localhost:8080/try2_war_exploded/getVehicleList',{
             method: 'POST',
@@ -14,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(response => response.json())
             .then(data => {
                 data.list.forEach(vehical => {
-                row += `<div class="vehicle-card vehicle-card-blue" style="--vehicle-card-color: #F5AF41;">
+                row += `<div class="vehicle-card vehicle-card-blue" id="${vehical.vehicleNo}" style="--vehicle-card-color: #F5AF41;">
                 <div class="vehicle-card-innerbox">
                     <img class="vehicle-card-img" src="../images/Bus/bus4.jpg" />
                     <div class="vehicle-card-textbox">
@@ -39,6 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 </div>
             </div>`
                 });
+                users = data.list;
 
                 document.querySelector(".vehicleSet").innerHTML = row;   
 
@@ -46,5 +51,20 @@ document.addEventListener("DOMContentLoaded", function () {
             .catch(error => {
                 console.error('Error:', error);
             });
+
+            searchInput.addEventListener("input", e => {
+                const value = e.target.value.toLowerCase()
+                users.forEach(data => {
+                  const isVisible =
+                      data.vehicleNo.toLowerCase().includes(value) ||
+                      data.type.toLowerCase().includes(value) ||
+                      data.vehicleBrand.toLowerCase().includes(value) ||
+                      data.model.toLowerCase().includes(value) ||
+                      data.startingPoint.toLowerCase().includes(value) ||
+                      data.endingPoint.toLowerCase().includes(value) ||
+                      data.trips.toLowerCase().includes(value)
+                      document.getElementById(data.vehicleNo).classList.toggle("hide", !isVisible)
+                    })
+                  })
     
   });
