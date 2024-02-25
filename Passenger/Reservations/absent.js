0bf24319fc884ca19414be873ec47817e4098ea2
Ravindu Hasanka <rhatu2000@gmail.com>
2024-02-25 22:36:03 +0530

    Passenger absent dropdown added and the profile added

--- Passenger/Reservations/absent.js
+++ Passenger/Reservations/absent.js
@@ -49,3 +49,69 @@ function createAbsent() {
   
 }
 
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    function getPayload(token) {
+        return JSON.parse(atob(token.split(".")[1]));
+    }
+
+    function checkCookie(cName) {
+        const name = cName + "=";
+        const cDecoded = decodeURIComponent(document.cookie); //to be careful
+        const cArr = cDecoded.split("; ");
+        let res;
+        cArr.forEach((val) => {
+            if (val.indexOf(name) === 0) res = val.substring(name.length);
+        });
+        return res;
+    }
+
+    console.log(checkCookie("jwt"))
+    const token = checkCookie("jwt");
+    
+    const payload = getPayload(token);
+    console.log("Payload",payload);
+    
+    passengerEmail = payload.id;
+  
+    let row =`<select name="" id="" class="Select-reservation">
+    <option value="Reservaton" selected disabled>Select relevant Reservation</option>`;
+
+    fetch('http://127.0.0.1:8080/try2_war_exploded/getReservationsByPassenger?passengerEmail='+passengerEmail,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: "include", })
+            .then(response => response.json())
+            .then(data => {
+                let i = 0;
+                let reservation = "";
+                let vehicle = "";
+                if(data.reservation == "No reservations"){
+                    alert("No reservations")
+                    //TO BE HANDLE
+                }
+                else{
+                    console.log(data);
+                    while (i < data.reservations.length) {
+                        reservation = data.reservations[i];
+                        vehicle = data.vehicles[i];
+                        console.log("reservation: "+reservation);
+                        console.log("vehicle : "+vehicle.vehicleNo);
+                        row += `<option value="01">${reservation.vehicleNo} (${vehicle.type})</option>`;
+                        i++;
+                    }
+
+                    row += `</select>`;
+
+
+                    document.querySelector(".reservation-dropdown").innerHTML = row; 
+                }  
+
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        });
